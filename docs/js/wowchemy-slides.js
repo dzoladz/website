6bcaff921470e94d46662f4173ddeae271e5aa63
Derek Zoladz <derek@derekzoladz.com>
2022-11-23 16:09:11 -0500

    Build 11-23-2022a

--- docs/js/wowchemy-slides.js
+++ docs/js/wowchemy-slides.js
@@ -1,8 +1,8 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/var/folders/wt/19kc558j4xl08h4p9jqksbjh0000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210324194200-fda9f39d872e/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/9p/j5xyjdt93pq_wy8mvsnd65300000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210324194200-fda9f39d872e/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
